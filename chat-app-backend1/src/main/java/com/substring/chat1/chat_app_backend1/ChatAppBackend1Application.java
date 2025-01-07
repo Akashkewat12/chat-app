98be6cfccbd4f940c12dd2d901e0b80a865082d7
Akashkewat12 <akashbind.bca.15@gmail.com>
2025-01-08 00:54:24 +0530

    my first commit

---
+++ chat-app-backend1/src/main/java/com/substring/chat1/chat_app_backend1/ChatAppBackend1Application.java
@@ -0,0 +1,13 @@
+package com.substring.chat1.chat_app_backend1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAppBackend1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatAppBackend1Application.class, args);
+	}
+
+}
---
+++ chat-app-backend1/src/test/java/com/substring/chat1/chat_app_backend1/ChatAppBackend1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.substring.chat1.chat_app_backend1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppBackend1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
